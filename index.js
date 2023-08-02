972cef97d6c80e511e6324e947a1bbbf47236f3d
Nithin Somagani <141116058+Nithin-114@users.noreply.github.com>
2023-08-02 12:41:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,37 +30,37 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "w":
+    case "#":
       var audio = new Audio('sounds/tom-1.mp3');
       audio.play();
       break;
 
-    case "a":
+    case "N":
       var audio = new Audio('sounds/tom-2.mp3');
       audio.play();
       break;
 
-    case "s":
+    case "I":
       var audio = new Audio('sounds/tom-3.mp3');
       audio.play();
       break;
 
-    case "d":
+    case "T":
       var audio = new Audio('sounds/tom-4.mp3');
       audio.play();
       break;
 
-    case "j":
+    case "H":
       var audio = new Audio('sounds/snare.mp3');
       audio.play();
       break;
 
-    case "k":
+    case "I":
       var audio = new Audio('sounds/crash.mp3');
       audio.play();
       break;
 
-    case "l":
+    case "N":
       var audio = new Audio('sounds/kick-bass.mp3');
       audio.play();
       break;
